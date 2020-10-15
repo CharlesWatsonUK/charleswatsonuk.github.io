a1167ba0ff26f605436118b4f7faf36781d0102a
Charles Watson <charles.w9788@googlemai.com>
2020-10-15 18:26:08 +0100

    version-1.0.0

---
+++ precache-manifest.f4c9f4bd1cccb703f5f416f5c632cd33.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48cea50f31f3fcb5a6a0d46b031ba1b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de6ac2279c7e2a544789",
+    "url": "/static/css/2.e6f51e71.chunk.css"
+  },
+  {
+    "revision": "0f0e862011fac1430254",
+    "url": "/static/css/main.feaa3322.chunk.css"
+  },
+  {
+    "revision": "de6ac2279c7e2a544789",
+    "url": "/static/js/2.ab4b8d86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ab4b8d86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f0e862011fac1430254",
+    "url": "/static/js/main.3b37b62f.chunk.js"
+  },
+  {
+    "revision": "8da32427f6dff9b5abfa",
+    "url": "/static/js/runtime-main.99e80802.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "4536688812e6c57dd97c665e6af761fa",
+    "url": "/static/media/google_fonts_titillium_web_latin.45366888.woff2"
+  },
+  {
+    "revision": "e8ca8d0963a49436fe774f5da0f4e3c8",
+    "url": "/static/media/google_fonts_titillium_web_latin_ext.e8ca8d09.woff2"
+  },
+  {
+    "revision": "37b86766ddbc9b63b5ca93202600ef42",
+    "url": "/static/media/me.37b86766.jpg"
+  }
+]);
